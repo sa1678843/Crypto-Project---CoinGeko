5d12258558f6a7c7cdced1e1039d3fb8f3fd4ac4
Sachih Kumar <sa1678854@gmail.com>
2025-01-21 23:02:45 +0530

    Added react query config

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
+import { data } from "autoprefixer";
 
 
 function CoinTable(){
+    const  [page, setPage] = useState(1);
+
+    const { isLoading, isError, error} =  useQuery(['coins', page], () => fetchCoinData(page, 'usd'),{
+      retry: 2,
+      retryDelay: 1000,
+      cacheTime: 1000 * 60 * 2,
+
+     });
+
+    
+     useEffect(()=>{
+      console.log(data)
+     }, [data]);
+     if(isLoading){
+      return <div>Loading...</div>
+     }
+     if(isError){
+      return <div>Error: {error.message}</div>
+     }
     
     return (
         <>
         
-        CoinTable
+        CoinTable <button onClick={() => setPage(page+1)}>Click</button>{page}
        
         </>
         
