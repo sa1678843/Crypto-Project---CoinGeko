24b106cc8e4e52080c93e281d68d952b8a33e7bd
Sachih Kumar <sa1678854@gmail.com>
2025-01-25 20:16:09 +0530

    coin table UI with cache

--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -5,8 +5,8 @@ try{
 
 
     const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`) ;
-    console.log(response)
-    return response
+    
+    return response.data
 } catch(error){
 console.error(error)
 return null;
